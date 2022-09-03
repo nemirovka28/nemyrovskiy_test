e3760e20bc20a0f6ecd3f52573823ff99acc4540
Aleksandr <104426761+nemirovka28@users.noreply.github.com>
2022-09-03 09:21:06 +0300

    add new file

--- src/js/price.js
+++ src/js/price.js
@@ -1,25 +1,29 @@
-const checkbox = document.querySelector('#checkbox')
-      checkbox.addEventListener('input', onClick);
-const monthly = document.querySelector('.monthly');
-      monthly.classList.add('is-active')
-const yearly = document.querySelector('.yearly');
-const price1 = document.querySelector('#price1');
-const price2 = document.querySelector('#price2');
-const price3 = document.querySelector('#price3');
+const refs = {
+    yearly: document.querySelector('.yearly'),
+    price1: document.querySelector('#price1'),
+    price2: document.querySelector('#price2'),
+    price3: document.querySelector('#price3'),
+    monthly: document.querySelector('.monthly'),
+    checkbox: document.querySelector('#checkbox')
+  };
 
+  refs.checkbox.addEventListener('input', onClick);
+
+  refs.monthly.classList.add('is-active')
+    
     export default function onClick (e) {
       
         if(e.target.checked){
-            price1.textContent = '$12'
-            price2.textContent = '$36'
-            price3.textContent = '$56'
-            yearly.classList.add('is-active')
-            monthly.classList.remove('is-active')
+            refs.price1.textContent = '$12'
+            refs.price2.textContent = '$36'
+            refs.price3.textContent = '$56'
+            refs.yearly.classList.add('is-active')
+            refs.monthly.classList.remove('is-active')
         } else {
-            price1.textContent = '$19'
-            price2.textContent = '$54'
-            price3.textContent = '$89'
-            yearly.classList.remove('is-active')
-            monthly.classList.add('is-active')
+            refs.price1.textContent = '$19'
+            refs.price2.textContent = '$54'
+            refs.price3.textContent = '$89'
+            refs.yearly.classList.remove('is-active')
+            refs.monthly.classList.add('is-active')
         }
      }
